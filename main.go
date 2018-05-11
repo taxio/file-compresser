9bf90c01e9a5afc31c5f7711961d37322eb6a3de
taxio <taxio.2029@gmail.com>
2018-05-11 16:29:29 +0900

    file read

---
+++ main.go
@@ -0,0 +1,31 @@
+//package file_compresser
+package main
+
+import (
+	"os"
+	"bufio"
+	"fmt"
+)
+
+func main(){
+	var fp *os.File
+	var err error
+
+	if len(os.Args) < 2 {
+		fp = os.Stdin
+	}else {
+		fp, err = os.Open(os.Args[1])
+		if err != nil {
+			panic(err)
+		}
+		defer fp.Close()
+	}
+
+	scanner := bufio.NewScanner(fp)
+	for scanner.Scan(){
+		fmt.Println(scanner.Text())
+	}
+	if err := scanner.Err(); err != nil {
+		panic(err)
+	}
+}
